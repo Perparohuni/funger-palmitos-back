9ce251f9e8385505ff4b353dabde64c94ff6ce36
Perparohuni <baronp617@gmail.com>
2026-09-01 19:37:32 -0300

    Agora o front recebe as mensagens de erro do back e as mostra em um toast, também estou passando os erros de SQl para o front

--- src/main/java/com/rpg/back/config/GlobalExceptionHandler.java
+++ src/main/java/com/rpg/back/config/GlobalExceptionHandler.java
@@ -7,11 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RPGException.class)
     public ResponseEntity<ErroRespostaDTO> rpgException(
             RPGException e
+    ) {
+        ErroRespostaDTO resposta = new ErroRespostaDTO(e.getMessage(), 404);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resposta);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<ErroRespostaDTO> sqlException(
+            SQLException e
     ) {
         ErroRespostaDTO resposta = new ErroRespostaDTO(e.getMessage(), 404);
 
